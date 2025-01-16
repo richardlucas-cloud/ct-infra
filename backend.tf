7aed42b3c6b9e148042da4a8b73d64fe373406ac
Daniel Lucas <daniel_lucas@epam.com>
2025-01-16 16:33:40 +0530

    github adding init validation

--- backend.tf
+++ backend.tf
@@ -1,26 +1,18 @@
-variable "storage_account_name" {
-  type    = string
-  default = "tfstatestg0001"
-}
-
-variable "resource_group_name" {
-  type    = string
-  default = "tfstate-rg"
-}
-
 variable "env" {
   type    = string
   default = "dev"
 }
 
+
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = "tfstate" # Must be the name of the blob container
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestg0001"
+    container_name       = "tfstate"
     key                  = "${var.env}.tfstate"
+    tenant_id            = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
+    subscription_id      = "2e8978a4-2ee8-445a-8287-ec90e39c778d"
   }
 }
-
 
 #terraform init -backend-config="resource_group_name=RG-01" -backend-config="storage_account_name=terraform00002" -backend-config="client_id=$ARM_CLIENT_ID" -backend-config="client_secret=$ARM_CLIENT_SECRET"  -backend-config="subscription_id=$ARM_SUBSCRIPTION_ID" -backend-config="tenant_id=$ARM_TENANT_ID"
